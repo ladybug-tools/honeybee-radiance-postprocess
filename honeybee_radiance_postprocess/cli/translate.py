03951415f141fc27af1b772a3bbd09020b83aabe
Mikkel Pedersen <mkklpdrsn@gmail.com>
2022-05-27 08:46:39 +0200

    fix(translate): Add command to convert from txt to npy

--- honeybee_radiance_postprocess/cli/translate.py
+++ honeybee_radiance_postprocess/cli/translate.py
@@ -12,7 +12,7 @@ def translate():
     pass
 
 
-@translate.command('npy-to-text')
+@translate.command('npy-to-txt')
 @click.argument(
     'npy-file', type=click.Path(exists=True, dir_okay=False, resolve_path=True)
 )
@@ -26,7 +26,7 @@ def translate():
     '--format', '-fmt', help='Output format for each element in the array',
     default='%.7e', show_default=True
 )
-def npy_to_text(
+def npy_to_txt(
     npy_file, name, extension, format
         ):
     """Convert a npy file to text file.
@@ -44,6 +44,36 @@ def npy_to_text(
 
     except Exception:
         _logger.exception('Converting npy file to text file failed.')
+        sys.exit(1)
+    else:
+        sys.exit(0)
+
+
+@translate.command('txt-to-npy')
+@click.argument(
+    'txt-file', type=click.Path(exists=True, dir_okay=False, resolve_path=True)
+)
+@click.option(
+    '--name', '-n', help='Output file name.', default='output', show_default=True
+)
+def txt_to_npy(
+    txt_file, name
+        ):
+    """Convert a text file to npy file.
+
+    This function reads a space or tab separated text file saves it as a NumPy file. As
+    an example the input file could be the annual illuminance values.
+
+    \b
+    Args:
+        txt-file: Path to text file.
+    """
+    try:
+        array = np.loadtxt(txt_file, dtype=np.float32)
+        np.save(name, array)
+
+    except Exception:
+        _logger.exception('Converting text file to npy file failed.')
         sys.exit(1)
     else:
         sys.exit(0)
