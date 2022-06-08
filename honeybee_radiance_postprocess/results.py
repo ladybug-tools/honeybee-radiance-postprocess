58337d9c4cb45c5914d230e149220cbabf1661eb
Mikkel Pedersen <mkklpdrsn@gmail.com>
2022-06-08 14:12:59 +0200

    fix(results): Build an array from states

--- honeybee_radiance_postprocess/results.py
+++ honeybee_radiance_postprocess/results.py
@@ -3,6 +3,7 @@ from pathlib import Path
 
 from honeybee_radiance.postprocess.annual import (_process_input_folder,
     filter_schedule_by_hours, generate_default_schedule)
+from honeybee_radiance_postprocess.metrics import da_array2d
 from .util import occupancy_filter
 
 
@@ -166,6 +167,205 @@ class Results(_ResultsFolder):
         of valid states, e.g., [0, 1, 2, ...]."""
         return self._valid_states
 
+    def daylight_autonomy(
+        self, threshold: float = 300, states: list = None, grids_filter: str = '*'):
+
+        grids_info = self._filter_grids(grids_filter=grids_filter)
+
+        for grid_info in grids_info:
+            grid_id = grid_info['identifier']
+            array = self.array_from_states(grid_info, states=states, type='total')
+            array_filter = np.apply_along_axis(
+                occupancy_filter, 1, array, mask=self.occ_mask)
+            results = da_array2d(
+                array_filter, total_occ=self.total_occ, threshold=threshold)
+            # do something here...
+
+    def _load_array(self, grid_id: str, light_path: str, state: int = 0,
+                    type: str = 'total', extension: str = '.npy'):
+        """Load a NumPy file to an array.
+        
+        This method will also update the arrays property value.
+        
+        Args:
+            grid_id: Grid identifier.
+            light_path: Light path identifier.
+            state: Integer of the state. E.g., 0 for the default state
+            type: Which type of result to return a file for. E.g., 'total' for total
+                illuminance or 'direct' for direct illuminance.
+            extension: File extension. (Default: .npy).
+        
+        Returns:
+            A NumPy array from a NumPy file.
+        """
+
+        def merge_dicts(array_dict, arrays):
+            for key, value in array_dict.items():
+                if isinstance(value, dict):
+                    node = arrays.setdefault(key, {})
+                    merge_dicts(value, node)
+                else:
+                    arrays[key] = value
+            return arrays
+
+        state_identifier = self._state_identifier(light_path, state=state)
+        file = self._get_file(grid_id, light_path, state_identifier, type,
+                              extension=extension)
+        array = np.load(file)
+
+        array_dict = {grid_id: {light_path: {state: {type: array}}}}
+        arrays = merge_dicts(array_dict, self.arrays)
+        self.arrays = arrays
+
+        return array
+
+    def _state_identifier(self, light_path: str, state: int = 0):
+        """Get the state identifier from a light path and state integer.
+        
+        Args:
+            light_path: Light path identifier.
+            state: Integer of the state. E.g., 0 for the default state. State integer
+                must be minimum 0. -1 for off cannot return a state identifier.
+        
+        Returns:
+            State identifier. For static apertures the identifier is
+            '__static_apertures__', and for other light paths it is the light path
+            identifier preceded by the state integer, e.g., '0_light_path'.
+        """
+        if state < 0:
+            raise ValueError('State integer must be minimum 0. Received %s' % state)
+        # TODO: Figure out if there is a better way to handle the states.
+        # I.e., state integer <--> state identifier.
+        if light_path == 'static_apertures':
+            state_identifer = '__static_apertures__'
+        elif light_path in self.light_paths:
+            state_identifer = str(state) + '_' + light_path
+        else:
+            raise ValueError(
+                'Light path is not valid. Valid light paths are any of %s. Received %s.'
+                % (self.light_paths, light_path))
+        
+        return state_identifer
+
+    def _get_file(self, grid_id: str, light_path: str, state_identifier: str,
+                  type: str = 'total', extension: str = '.npy'):
+        """Return the path of a results file.
+        
+        Args:
+            grid_id: Grid identifier.
+            light_path: Light path identifier.
+            state_identifier: State identifier.
+            type: Which type of result to return a file for. E.g., 'total' for total
+                illuminance or 'direct' for direct illuminance.
+            extension: File extension. (Default: .npy).
+        
+        Returns:
+            Path to a NumPy file.
+        """
+        if light_path == 'static_apertures':
+            file = Path(self.folder, light_path, '__static_apertures__', type,
+                        grid_id + extension)
+        else:
+            file = Path(self.folder, light_path, state_identifier, type,
+                        grid_id + extension)
+        
+        return file
+
+    def array_from_states(
+        self, grid_info: str, states: list = None, type: str = 'total'):
+        """Create an array for a given grid by the states settings.
+        
+        Args:
+            grid_info: Grid information of the grid.
+            states: A list of states. Either a list of one combination of settings or a
+                list of 8760 combinations for each hour.
+            type: Which type of result to create an array for. E.g., 'total' for total
+                illuminance or 'direct' for direct illuminance.
+        
+        Returns:
+            A NumPy arrray based on the states settings.
+        """
+        grid_id = grid_info['identifier']
+        light_paths = grid_info['light_path']
+        arrays = []
+        if not states:
+            states = [self.default_states]
+        if len(states) == 1:
+            st = states[0]
+            for light_path in light_paths:
+                light_path = light_path[0]
+                try:
+                    state = st[light_path]
+                except:
+                    state = 0
+                if light_path == 'static_apertures':
+                    if state == 0:
+                        try:
+                            array = self.arrays[grid_id][light_path]['__static_apertures__'][type]
+                        except:
+                            array = self._load_array(grid_id, light_path, state, type)
+                        arrays.append(array)
+                    elif state == -1:
+                        pass
+                    else:
+                        raise ValueError('State of static apertures must be either 0 for on '
+                        'or -1 for off. Received state %s.' % state)
+                else:
+                    if state in self.valid_states[light_path]:
+                        try:
+                            array = self.arrays[grid_id][light_path][str(state) + '_' + light_path][type]
+                        except:
+                            array = self._load_array(grid_id, light_path, state, type)
+                        arrays.append(array)
+                    elif state == -1:
+                        pass
+                    else:
+                        raise ValueError('State of %s must be any of %s for on '
+                        'or -1 for off. Received state %s.' % (light_path, self.valid_states[light_path], state))
+            if arrays:
+                array = sum(arrays)
+            else:
+                array = []
+
+        elif len(states) == 8760:
+            states = np.array(states)[list(map(int, self.sun_up_hours))].tolist()
+            for n, st in enumerate(states):
+                st_arrays = []
+                for light_path in light_paths:
+                    light_path = light_path[0]
+                    try:
+                        state = st[light_path]
+                    except:
+                        state = 0
+                    if light_path == 'static_apertures':
+                        if state == 0:
+                            try:
+                                array = self.arrays[grid_id][light_path]['__static_apertures__'][type]
+                            except:
+                                array = self._load_array(grid_id, light_path, state, type)
+                            st_arrays.append(np.take(array, n, axis=1))
+                        elif state == -1:
+                            st_arrays.append(np.zeros(grid_info['count']))
+                        else:
+                            raise ValueError('State of static apertures must be either 0 for on '
+                            'or -1 for off. Received state %s.' % state)
+                    else:
+                        if state in self.valid_states[light_path]:
+                            try:
+                                array = self.arrays[grid_id][light_path][str(state) + '_' + light_path][type]
+                            except:
+                                array = self._load_array(grid_id, light_path, state, type)
+                            st_arrays.append(np.take(array, n, axis=1))
+                        elif state == -1:
+                            st_arrays.append(np.zeros(grid_info['count']))
+                        else:
+                            raise ValueError('State of %s must be any of %s for on '
+                            'or -1 for off. Received state %s.' % (light_path, self.valid_states[light_path], state))
+                arrays.append(sum(st_arrays))
+            array = np.asarray(arrays).transpose()
+
+        return array
+
     def _update_occ(self):
         """Set properties related to occupancy."""
         sun_up_hours, schedule = [self.sun_up_hours, self.schedule]
@@ -173,7 +373,7 @@ class Results(_ResultsFolder):
             filter_schedule_by_hours(sun_up_hours=sun_up_hours, schedule=schedule)
         self._occ_mask = np.array(self.occ_pattern)
 
-    def _filter_grids(self, grids_filter='*'):
+    def _filter_grids(self, grids_filter: str = '*'):
         """Return grids information."""
         if grids_filter != '*':
             grids_info, _ = _process_input_folder(self.folder, grids_filter)
@@ -221,17 +421,17 @@ class Results(_ResultsFolder):
             'Room2_North2': [0, 1]
         }
         """
-        arrays = dict()
+        valid_states = dict()
 
         for light_path in self.light_paths:
             if light_path == 'static_apertures':
-                arrays[light_path] = [0]
+                valid_states[light_path] = [0]
                 continue
             light_path_folder = Path(self.folder, light_path)
             for state_folder in Path(light_path_folder).iterdir():
-                if light_path in arrays:
-                    arrays[light_path].append(int(state_folder.stem.split('_')[0]))
+                if light_path in valid_states:
+                    valid_states[light_path].append(int(state_folder.stem.split('_')[0]))
                 else:
-                    arrays[light_path] = [int(state_folder.stem.split('_')[0])]
+                    valid_states[light_path] = [int(state_folder.stem.split('_')[0])]
 
-        return arrays
+        return valid_states
