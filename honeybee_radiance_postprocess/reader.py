6c921c293aa1acf1f734a19345617d0dde577bc9
Mikkel Pedersen <mkklpdrsn@gmail.com>
2022-07-06 18:07:40 +0200

    fix(reader) Add function to read ascii files to array

--- honeybee_radiance_postprocess/reader.py
+++ honeybee_radiance_postprocess/reader.py
@@ -46,8 +46,8 @@ def feather_to_dataframe(filepath: str) -> pd.DataFrame:
 
 
 def binary_to_array(
-        binary_file: str, nrows: int = None, ncols: int = None, ncomp: int = None
-        ) -> np.ndarray:
+        binary_file: str, nrows: int = None, ncols: int = None, ncomp: int = None,
+        line_count: int = 0) -> np.ndarray:
     """Read a Radiance binary file as a NumPy array.
     
     Args:
@@ -55,6 +55,8 @@ def binary_to_array(
         nrows: Number of rows in the Radiance file.
         ncols: Number of columns in the Radiance file.
         ncomp: Number of components of each element in the Radiance file.
+        line_count: Number of lines to skip in the input file. Usually used to
+            skip the header.
     
     Returns:
         A NumPy array.
@@ -64,13 +66,45 @@ def binary_to_array(
         if (nrows or ncols or ncomp) is None:
             # get nrows, ncols and header line count
             nrows, ncols, ncomp, line_count = binary_mtx_dimension(binary_file)
-            # skip first n lines from reader
-            for i in range(line_count):
-                reader.readline()
-            
-            if ncomp != 1:
-                array = np.fromfile(reader, dtype=np.float32).reshape(nrows, ncols, ncomp)
-            else:
-                array = np.fromfile(reader, dtype=np.float32).reshape(nrows, ncols)
+        # skip first n lines from reader
+        for i in range(line_count):
+            reader.readline()
+
+        array = np.fromfile(reader, dtype=np.float32)
+        if ncomp != 1:
+            array = array.reshape(nrows, ncols, ncomp)
+        else:
+            array = array.reshape(nrows, ncols)
+
+    return array
+
+
+def ascii_to_array(
+        ascii_file: str, nrows: int = None, ncols: int = None, ncomp: int = None,
+        line_count: int = 0) -> np.ndarray:
+    """Read a Radiance ascii file as a NumPy array.
+    
+    Args:
+        ascii_file: Path to ascii Radiance file.
+        nrows: Number of rows in the Radiance file.
+        ncols: Number of columns in the Radiance file.
+        ncomp: Number of components of each element in the Radiance file.
+        line_count: Number of lines to skip in the input file. Usually used to
+            skip the header.
+    
+    Returns:
+        A NumPy array.
+    """
+    with open(ascii_file, 'r') as reader:
+        if (nrows or ncols or ncomp) is None:
+            # get nrows, ncols and header line count
+            # we can reuse binary_mtx_dimension though the input file is ascii
+            nrows, ncols, ncomp, line_count = binary_mtx_dimension(ascii_file) 
+
+        array = np.loadtxt(reader, dtype=np.float32, skiprows=line_count)
+        if ncomp != 1:
+            array = array.reshape(nrows, ncols, ncomp)
+        else:
+            array = array.reshape(nrows, ncols)
 
     return array
