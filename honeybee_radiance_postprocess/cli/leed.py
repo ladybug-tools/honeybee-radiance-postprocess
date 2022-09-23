a075d2c0435b227c0bb025c9456b53f5edad88ec
Mikkel Pedersen <mkklpdrsn@gmail.com>
2022-09-23 11:22:53 +0200

    fix(leed): Set default value if aperture group is missing in JSON file

--- honeybee_radiance_postprocess/cli/leed.py
+++ honeybee_radiance_postprocess/cli/leed.py
@@ -6,6 +6,7 @@ import os
 import click
 
 from ..leed import leed_option_1
+from ..results import Results
 
 _logger = logging.getLogger(__name__)
 
@@ -15,7 +16,7 @@ def leed():
     pass
 
 
-@leed.command('daylight_option_1')
+@leed.command('daylight-option-1')
 @click.argument(
     'folder',
     type=click.Path(exists=True, file_okay=False, dir_okay=True, resolve_path=True)
@@ -32,8 +33,10 @@ def leed():
 @click.option(
     '--shade-transmittance-file', '-stf', help='A JSON file with a dictionary '
     'where aperture groups are keys, and the value for each key is the shade '
-    'transmittance. Values for shade transmittance must be 1 > value > 0.',
-    default=None, show_default=True,
+    'transmittance. Values for shade transmittance must be 1 > value > 0. '
+    'If any aperture groups are missing in the JSON file, its shade transmittance '
+    'value will be set to the value of the shade transmittance option (0.2 by '
+    'default).', default=None, show_default=True,
     type=click.Path(exists=False, file_okay=True, dir_okay=False, resolve_path=True)
 )
 @click.option(
@@ -46,6 +49,12 @@ def daylight_option_1(
 ):
     """Calculate credits for LEED v4.1 Daylight Option 1.
 
+    Use the shade-transmittance option to set a shade transmittance values for
+    aperture groups. The shade-transmittance-file option takes precedence over
+    the shade-transmittance, however, if any aperture groups are missing in the
+    JSON file given to the shade-transmittance-file option, the value from
+    shade-transmittance will be used for those aperture groups.
+
     \b
     Args:
         folder: Results folder. This folder is an output folder of annual daylight
@@ -53,8 +62,14 @@ def daylight_option_1(
     """
     if shade_transmittance_file and os.path.isfile(shade_transmittance_file):
         with open(shade_transmittance_file) as json_file:
-            shade_transmittance = json.load(json_file)
-
+            shd_trans = json.load(json_file)
+        results = Results(folder)
+        # check if aperture groups are missing in json file
+        for light_path in results.light_paths:
+            if (not light_path in shd_trans and
+                light_path != '__static_apertures__'):
+                shd_trans[light_path] = shade_transmittance
+        shade_transmittance = shd_trans
     try:
         leed_option_1(
             folder, grids_filter=grids_filter,
