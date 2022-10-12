87a480f225dfa83bf459d00b4986813e8e641be9
Mikkel Pedersen <mkklpdrsn@gmail.com>
2022-10-12 15:30:02 +0200

    fix(leed): Add warnings to summary JSON

--- honeybee_radiance_postprocess/leed.py
+++ honeybee_radiance_postprocess/leed.py
@@ -4,7 +4,6 @@ from pathlib import Path
 from collections import defaultdict
 import json
 import itertools
-import warnings
 import numpy as np
 
 from honeybee.model import Model
@@ -17,9 +16,99 @@ from .results import Results
 from .util import filter_array, recursive_dict_merge
 
 
+def _create_grid_summary(
+    grid_id, sda_grid, ase_grid, pass_sda, pass_ase, total_floor,
+    area_weighted=True):
+    """Create a LEED summary for a single grid.
+
+    Args:
+        grid_id: Identifier of grid.
+        sda_grid: Spatial Daylight Autonomy.
+        ase_grid: Annual Sunlight Exposure.
+        pass_sda: The percentage of the sensor points or floor area that
+            passes sDA.
+        pass_ase: The percentage of the sensor points or floor area that
+            passes ASE.
+        total_floor: The number of sensor points or floor area.
+        area_weighted: Boolean to determine if the results are area
+            weighted. Defaults to True.
+
+    Returns:
+        Tuple:
+        -   summary_grid: Summary of each grid individually.
+        -   pass_sda: The percentage of the sensor points or floor area
+                that passes sDA. Note that this will be set to 0 if the
+                grid does not pass ASE.
+        -   pass_ase: The percentage of the sensor points or floor area
+                that passes ASE.
+    """
+    grid_summary = {
+        grid_id: {}
+    }
+    __pass_sda = pass_sda
+    __pass_ase = pass_ase
+    if 10 < ase_grid < 20:
+        ase_note = (
+            'The Annual Sunlight Exposure is greater than 10% for grid: '
+            f'{grid_id}. Identify in writing how the space is designed to '
+            'address glare.'
+        )
+        grid_summary[grid_id]['ase_note'] = ase_note
+    elif ase_grid >= 20:
+        ase_warning = (
+            'The Annual Sunlight Exposure is 20% or larger. The space '
+            'does not qualify in the calculation of Spatial Daylight '
+            'Autonomy.'
+        )
+        grid_summary[grid_id] = {
+            'ase_warning': ase_warning
+        }
+        sda_grid = pass_sda = 0
+
+    if area_weighted:
+        _grid_summary = {
+            grid_id: {
+                'ase': round(ase_grid, 2),
+                'sda': round(sda_grid, 2),
+                'floor_area_passing_ase': round(__pass_ase, 2),
+                'floor_area_passing_sda': round(__pass_sda, 2),
+                'total_floor_area': total_floor
+            }
+        }
+    else:
+        _grid_summary = {
+            grid_id: {
+                'ase': round(ase_grid, 2),
+                'sda': round(sda_grid, 2),
+                'sensor_count_passing_ase': int(round(__pass_ase, 2)),
+                'sensor_count_passing_sda': int(round(__pass_sda, 2)),
+                'total_sensor_count': total_floor
+            }
+        }
+
+    recursive_dict_merge(grid_summary, _grid_summary)
+
+    return grid_summary, pass_sda, pass_ase
+
 def _leed_summary(
-    pass_ase_grids: list, pass_sda_grids: list, grid_areas: list,
-    grids_info: list) -> Tuple[dict, dict]:
+    pass_ase_grids: list, pass_sda_grids: list, grids_info: list,
+    grid_areas: list = None) -> Tuple[dict, dict]:
+    """_summary_
+
+    Args:
+        pass_ase_grids: A list where each sublist is a list of True/False that
+            tells if each sensor point passes ASE.
+        pass_sda_grids: A list where each sublist is a list of True/False that
+            tells if each sensor point passes sDA.
+        grids_info: A list of grid information.
+        grid_areas: An optional list where each sublist is the area of each
+            sensor point.
+
+    Returns:
+        Tuple:
+        -   summary: Summary of of all grids combined.
+        -   summary_grid: Summary of each grid individually.
+    """
     summary = {}
     summary_grid = {}
 
@@ -32,24 +121,23 @@ def _leed_summary(
             total_grid_area = grid_area.sum()
             area_pass_ase = grid_area[pass_ase].sum()
             area_pass_sda = grid_area[pass_sda].sum()
+            ase_grid = area_pass_ase / total_grid_area * 100
+            sda_grid = area_pass_sda / total_grid_area * 100
             # grid summary
-            grid_summary = {
-                grid_id: {
-                    'ase': area_pass_ase / total_grid_area,
-                    'sda': area_pass_sda / total_grid_area,
-                    'floor_area_passing_ase': area_pass_ase,
-                    'floor_area_passing_sda': area_pass_sda,
-                    'total_floor_area': total_grid_area
-                }
-            }
+            grid_summary, area_pass_sda, area_pass_ase = \
+                _create_grid_summary(
+                    grid_id, sda_grid, ase_grid, area_pass_sda, area_pass_ase,
+                    total_grid_area, area_weighted=True
+                )
+
             recursive_dict_merge(summary_grid, grid_summary)
 
             total_area +=total_grid_area
             total_area_pass_ase += area_pass_ase
             total_area_pass_sda += area_pass_sda
 
-        summary['ase'] = total_area_pass_ase / total_area
-        summary['sda'] = total_area_pass_sda / total_area
+        summary['ase'] = round(total_area_pass_ase / total_area * 100, 2)
+        summary['sda'] = round(total_area_pass_sda / total_area * 100, 2)
         summary['floor_area_passing_ase'] = total_area_pass_ase
         summary['floor_area_passing_sda'] = total_area_pass_sda
         summary['total_floor_area'] = total_area
@@ -63,24 +151,23 @@ def _leed_summary(
             grid_count = grid_info['count']
             sensor_count_pass_ase = pass_ase.sum()
             sensor_count_pass_sda = pass_sda.sum()
+            ase_grid = sensor_count_pass_ase / grid_count * 100
+            sda_grid = sensor_count_pass_sda / grid_count * 100
             # grid summary
-            grid_summary = {
-                grid_id: {
-                    'ase': sensor_count_pass_ase / grid_count,
-                    'sda': sensor_count_pass_sda / grid_count,
-                    'sensor_count_passing_ase': int(sensor_count_pass_ase),
-                    'sensor_count_pass_sda': int(sensor_count_pass_sda),
-                    'total_sensor_count': grid_count
-                }
-            }
+            grid_summary, sensor_count_pass_sda, sensor_count_pass_ase = \
+                _create_grid_summary(
+                    grid_id, sda_grid, ase_grid, sensor_count_pass_sda,
+                    sensor_count_pass_ase, grid_count, area_weighted=False
+                )
+
             recursive_dict_merge(summary_grid, grid_summary)
 
             total_sensor_count += grid_count
             total_sensor_count_pass_ase += sensor_count_pass_ase
             total_sensor_count_pass_sda += sensor_count_pass_sda
 
-        summary['ase'] = total_sensor_count_pass_ase / total_sensor_count
-        summary['sda'] = total_sensor_count_pass_sda / total_sensor_count
+        summary['ase'] = round(total_sensor_count_pass_ase / total_sensor_count * 100, 2)
+        summary['sda'] = round(total_sensor_count_pass_sda / total_sensor_count * 100, 2)
         summary['sensor_count_passing_ase'] = int(total_sensor_count_pass_ase)
         summary['sensor_count_passing_sda'] = int(total_sensor_count_pass_sda)
         summary['total_sensor_count'] = total_sensor_count
@@ -194,7 +281,7 @@ def leed_states_schedule(
         grid_comply = np.where(np.all(array_combinations==np.NINF, axis=0))[0]
         if grid_comply.size != 0:
             fail_to_comply[grid_id] = \
-                [results.datetimes[hoy] for hoy in grid_comply]
+                [int(hoy) for hoy in grid_comply]
 
         array_combinations_filter = \
             np.apply_along_axis(filter_array, 1, array_combinations, occ_mask)
@@ -288,15 +375,6 @@ def leed_option_1(
             'Custom input for argument states_schedule is not yet implemented.'
             )
 
-    if fail_to_comply:
-        warning_msg = (
-            'For some hours it is not possible to keep the percentage of '
-            'sensors that receive more than 1000 direct illuminance at 2% or '
-            'below. We should do something about this, but for now I just let '
-            'it continue.'
-            )
-        warnings.warn(warning_msg)
-
     # check to see if there is a HBJSON with sensor grid meshes for areas
     grid_areas, units_conversion = [], 1
     for base_file in Path(results.folder).parent.iterdir():
@@ -329,13 +407,7 @@ def leed_option_1(
         # calculate ase per grid
         ase_grid, h_above = ase_array2d(
             array, occ_hours=occ_hours, direct_threshold=direct_threshold)
-        if ase_grid > 0.10:
-            warning_msg = (
-                'The Annual Sunlight Exposure is greater than 10% for grid: '
-                f'{grid_id}. Identify in writing how the space is designed to '
-                'address glare.'
-                )
-            warnings.warn(warning_msg)
+
         ase_grids.append(ase_grid)
         hours_above.append(h_above)
         pass_ase = (h_above > occ_hours)
@@ -368,19 +440,19 @@ def leed_option_1(
 
     # create summaries for all grids and each grid individually
     summary, summary_grid = _leed_summary(
-        pass_ase_grids, pass_sda_grids, grid_areas, grids_info)
+        pass_ase_grids, pass_sda_grids, grids_info, grid_areas=grid_areas)
 
     # credits
-    if summary['sda'] >= 0.75:
+    if summary['sda'] >= 75:
         summary['credits'] = 3
-    elif summary['sda'] >= 0.55:
+    elif summary['sda'] >= 55:
         summary['credits'] = 2
-    elif summary['sda'] >= 0.40:
+    elif summary['sda'] >= 40:
         summary['credits'] = 1
     else:
         summary['credits'] = 0
 
-    if (np.array(sda_grids) >= 0.55).all():
+    if all(grid_summary['sda'] >= 55 for grid_summary in summary_grid.values()):
         if summary['credits'] <= 2:
             summary['credits'] += 1
         else:
@@ -410,6 +482,10 @@ def leed_option_1(
             hours_above_file.parent.mkdir(parents=True, exist_ok=True)
             np.savetxt(da_file, da, fmt='%.2f')
             np.savetxt(hours_above_file, h_above, fmt='%.0f')
+
+        if fail_to_comply:
+            states_schedule_err_file = folder.joinpath('states_schedule_err.json')
+            states_schedule_err_file.write_text(json.dumps(fail_to_comply))
 
     return (summary, summary_grid, da_grids, hours_above, states_schedule,
             grids_info)
