50e3aaafb194d0b3b6716d856c523b4ce87ad340
Mikkel Pedersen <mkklpdrsn@gmail.com>
2022-12-07 18:12:00 +0100

    test(schedule): Add test for control schedules CLI command

---
+++ tests/cli/schedule_cli_test.py
@@ -0,0 +1,24 @@
+"""Test cli schedule module."""
+import os
+from click.testing import CliRunner
+
+from ladybug.futil import nukedir
+
+from honeybee_radiance_postprocess.cli.schedule import control_schedules
+
+
+def test_control_schedules():
+    runner = CliRunner()
+    folder = './tests/assets/results_folders/results_sample'
+    sub_folder = './tests/assets/temp/schedules'
+    cmd_args = [folder, '--sub-folder', sub_folder]
+
+    schedule_files = [
+        os.path.join(sub_folder, 'control_schedules/TestRoom_1.txt'),
+        os.path.join(sub_folder, 'control_schedules/TestRoom_2.txt')
+    ]
+    result = runner.invoke(control_schedules, cmd_args)
+    assert result.exit_code == 0
+    for schedule_file in schedule_files:
+        assert os.path.isfile(schedule_file)
+    nukedir(sub_folder, rmdir=True)
