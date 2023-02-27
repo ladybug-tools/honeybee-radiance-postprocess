124371b6ec81e58cb982cdd622d5a6a7817aedff
Mikkel Pedersen <mkklpdrsn@gmail.com>
2023-02-27 12:54:38 +0100

    fix(leed): Add vis metadata

--- honeybee_radiance_postprocess/leed.py
+++ honeybee_radiance_postprocess/leed.py
@@ -6,6 +6,10 @@ import json
 import itertools
 import numpy as np
 
+from ladybug.color import Colorset
+from ladybug.datatype.fraction import Fraction
+from ladybug.datatype.time import Time
+from ladybug.legend import LegendParameters
 from honeybee.model import Model
 from honeybee.units import conversion_factor_to_meters
 from honeybee_radiance.writer import _filter_by_pattern
@@ -553,3 +557,33 @@ def leed_option_one(
 
     return (summary, summary_grid, da_grids, hours_above, states_schedule,
             grids_info)
+
+
+def _leed_daylight_option_one_vis_metadata():
+    """Return visualization metadata for leed daylight option one."""
+    da_lpar = LegendParameters(min=0, max=100, colors=Colorset.annual_comfort())
+    ase_hrs_lpar = LegendParameters(min=0, max=250, colors=Colorset.original())
+    ase_pct_lpar = LegendParameters(min=0, max=100, colors=Colorset.original())
+
+    metric_info_dict = {
+        'da': {
+            'type': 'VisualizationMetaData',
+            'data_type': Fraction('Daylight Autonomy').to_dict(),
+            'unit': '%',
+            'legend_parameters': da_lpar.to_dict()
+        },
+        'ase_hours_above': {
+            'type': 'VisualizationMetaData',
+            'data_type': Time('Hours above direct threshold').to_dict(),
+            'unit': 'hr',
+            'legend_parameters': ase_hrs_lpar.to_dict()
+        },
+        'ase_percentage_above': {
+            'type': 'VisualizationMetaData',
+            'data_type': Fraction('Percentage above direct threshold').to_dict(),
+            'unit': '%',
+            'legend_parameters': ase_pct_lpar.to_dict()
+        },
+    }
+
+    return metric_info_dict
