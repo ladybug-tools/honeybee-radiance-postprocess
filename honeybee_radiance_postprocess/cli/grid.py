9504eac1f33c57d2d19e3686bb5389e8fdf5305d
Mikkel Pedersen <mkklpdrsn@gmail.com>
2023-05-18 18:07:19 +0200

    fix(grid): Add command to merge distributed metrics

--- honeybee_radiance_postprocess/cli/grid.py
+++ honeybee_radiance_postprocess/cli/grid.py
@@ -1,7 +1,6 @@
 """honeybee radiance postprocess grid commands."""
 import click
 import sys
-import os
 import logging
 import json
 import numpy as np
@@ -47,6 +46,54 @@ def merge_grid_folder(input_folder, output_folder, extension, dist_info):
         if dist_info and not Path(dist_info).is_file():
             dist_info = None
         restore_original_distribution(input_folder, output_folder, extension, dist_info)
+    except Exception:
+        _logger.exception('Failed to restructure data from folder.')
+        sys.exit(1)
+    else:
+        sys.exit(0)
+
+
+@grid.command('merge-folder-metrics')
+@click.argument(
+    'input-folder',
+    type=click.Path(exists=True, file_okay=False, dir_okay=True, resolve_path=True))
+@click.argument(
+    'output-folder',
+    type=click.Path(file_okay=False, dir_okay=True, resolve_path=True))
+@click.option(
+    '--dist-info', '-di',
+    help='An optional input for distribution information to put the grids back together '
+    '. Alternatively, the command will look for a _redist_info.json file inside the '
+    'folder.', type=click.Path(file_okay=True, dir_okay=False, resolve_path=True)
+)
+def merge_metrics_folder(input_folder, output_folder, dist_info):
+    """Restructure annual daylight metrics in a distributed folder.
+    
+    Since this command redistributes metrics it is expected that the input
+    folder has sub folder
+
+    \b
+    Args:
+        input_folder: Path to input folder.
+        output_folder: Path to the new restructured folder
+    """
+    try:
+        # handle optional case for Functions input
+        if dist_info and not Path(dist_info).is_file():
+            dist_info = None
+        extension_mapper = {
+            'da': 'da',
+            'cda': 'cda',
+            'udi': 'udi',
+            'udi_lower': 'udi',
+            'udi_upper': 'udi'
+        }
+        input_folder = Path(input_folder)
+        for metric in ['da', 'cda', 'udi', 'udi_lower', 'udi_upper']:
+            extension = extension_mapper[metric]
+            metric_folder = input_folder.joinpath(metric)
+            restore_original_distribution_metrics(
+                metric_folder, output_folder, metric, extension, dist_info)
     except Exception:
         _logger.exception('Failed to restructure data from folder.')
         sys.exit(1)
@@ -100,7 +147,6 @@ def restore_original_distribution(
             )
             st = src_info['st_ln']
             end = src_info['end_ln']
-
             try:
                 array = np.load(src_file)
             except Exception:
@@ -112,3 +158,58 @@ def restore_original_distribution(
         out_array = np.concatenate(out_arrays)
         # save numpy array, .npy extension is added automatically
         np.save(output_file, out_array)
+
+
+def restore_original_distribution_metrics(
+        input_folder, output_folder, metric, extension, dist_info=None):
+    """Restructure files to the original distribution based on the distribution info.
+    
+    It will assume that the files in the input folder are NumPy files. However,
+    if it fails to load the files as arrays it will try to load from binary
+    Radiance files to array.
+
+    Args:
+        input_folder: Path to input folder.
+        output_folder: Path to the new restructured folder
+        metric: Name of the metric to redistribute.
+        extension: Extension of the files to collect data from. For annual
+            daylight metrics the extension can be 'da', 'cda', or 'udi'.
+        dist_info: Path to dist_info.json file. If None, the function will try to load
+            ``_redist_info.json`` file from inside the input_folder. (Default: None).
+    """
+    if not dist_info:
+        _redist_info_file = Path(input_folder, '_redist_info.json')
+    else:
+        _redist_info_file = Path(dist_info)
+
+    assert _redist_info_file.is_file(), 'Failed to find %s' % _redist_info_file
+
+    with open(_redist_info_file) as inf:
+        data = json.load(inf)
+
+    # create output folder
+    output_folder = Path(output_folder)
+    if not output_folder.is_dir():
+        output_folder.mkdir()
+
+    for f in data:
+        output_file = Path(output_folder, metric, '%s.%s' % (f['identifier'], extension))
+        # ensure the new folder is created. in case the identifier has a subfolder
+        parent_folder = output_file.parent
+        if not parent_folder.is_dir():
+            parent_folder.mkdir()
+
+        out_arrays = []
+        for src_info in f['dist_info']:
+            src_file = Path(
+                input_folder, '%s.%s' % (src_info['identifier'], extension)
+            )
+            st = src_info['st_ln']
+            end = src_info['end_ln']
+            array = np.loadtxt(src_file)
+            slice_array = array[st:end+1]
+            out_arrays.append(slice_array)
+
+        out_array = np.concatenate(out_arrays)
+        # save array as txt file
+        np.savetxt(output_file, out_array, fmt='%.2f')
