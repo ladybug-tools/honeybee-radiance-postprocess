a5cecc0992f7f5474f5bfa2b873a8978b5bbaa07
Mikkel Pedersen <mkklpdrsn@gmail.com>
2023-11-03 11:31:59 +0100

    test(two_phase): Add test for add-remove-sky-matrix

--- tests/cli/two_phase_cli_test.py
+++ tests/cli/two_phase_cli_test.py
@@ -39,3 +39,21 @@ def test_rgb_to_illuminance_file():
     assert result.exit_code == 0
     assert total_file.exists()
     nukedir(output_folder, rmdir=False)
+
+
+def test_add_remove_sky_matrix():
+    runner = CliRunner()
+    total_mtx = './tests/assets/binary/sky.ill'
+    direct_mtx = './tests/assets/binary/sky_dir.ill'
+    direct_sunlight_mtx = './tests/assets/binary/sun.ill'
+    output_folder = Path('./tests/assets/temp')
+    total_file = output_folder.joinpath('total.npy')
+    direct_file = output_folder.joinpath('direct.npy')
+    cmd_args = [
+        total_mtx, direct_mtx, direct_sunlight_mtx, '--output-folder', output_folder
+    ]
+    result = runner.invoke(add_remove_sky_matrix, cmd_args)
+    assert result.exit_code == 0
+    assert total_file.exists()
+    assert direct_file.exists()
+    nukedir(output_folder, rmdir=False)
