c8a146c73b0d97e85848873d43c84e886a099ab4
Mikkel Pedersen <mkklpdrsn@gmail.com>
2023-11-13 13:58:36 +0100

    test(results): Add tests for AnnualDaylight

--- tests/results_test.py
+++ tests/results_test.py
@@ -3,13 +3,14 @@ from pathlib import Path
 import numpy as np
 
 from ladybug.futil import nukedir
-from honeybee_radiance_postprocess.results import Results
+from honeybee_radiance_postprocess.results.results import Results
 from honeybee_radiance_postprocess.dynamic import DynamicSchedule
+from honeybee_radiance_postprocess.results.annual_daylight import AnnualDaylight
 
 
 def test_results_sample_creation():
     folder = './tests/assets/results_folders/results_sample'
-    results = Results(folder)
+    results = AnnualDaylight(folder)
     assert len(results.grids_info) == 2
     assert len(results.sun_up_hours) == 4393
 
@@ -17,13 +18,13 @@ def test_results_sample_creation():
 def test_results_sample_annual_metrics():
     # annual_metrics will test all five metrics as well
     folder = './tests/assets/results_folders/results_sample'
-    results = Results(folder)
+    results = AnnualDaylight(folder)
     da, cda, udi, udi_lower, udi_upper, grids_info = results.annual_metrics()
 
 
 def test_results_sample_annual_metrics_static_states():
     folder = './tests/assets/results_folders/results_sample'
-    results = Results(folder)
+    results = AnnualDaylight(folder)
     light_paths = results.light_paths
     states = {}
     for light_path in light_paths:
@@ -42,7 +43,7 @@ def test_results_sample_annual_metrics_static_states():
 
 def test_results_sample_annual_metrics_dynamic_states():
     folder = './tests/assets/results_folders/results_sample'
-    results = Results(folder)
+    results = AnnualDaylight(folder)
     light_paths = results.light_paths
     states = {}
     for light_path in light_paths:
@@ -62,7 +63,7 @@ def test_results_sample_annual_metrics_dynamic_states():
 def test_results_sample_annual_metrics_dynamic_states_off():
     # set state to -1 for all light paths
     folder = './tests/assets/results_folders/results_sample'
-    results = Results(folder)
+    results = AnnualDaylight(folder)
     light_paths = results.light_paths
     states = {}
     for light_path in light_paths:
@@ -84,7 +85,7 @@ def test_results_sample_annual_metrics_dynamic_states_off():
 def test_results_sample_annual_metrics_to_folder():
     folder = Path('./tests/assets/results_folders/results_sample')
     target_folder = Path('./tests/assets/temp')
-    results = Results(folder)
+    results = AnnualDaylight(folder)
     results.annual_metrics_to_folder(target_folder)
     da_folder = Path(target_folder, 'da')
     cda_folder = Path(target_folder, 'cda')
@@ -102,7 +103,7 @@ def test_results_sample_annual_metrics_to_folder():
 def test_results_sample_average_values_to_folder():
     folder = Path('./tests/assets/results_folders/results_sample')
     target_folder = Path('./tests/assets/temp')
-    results = Results(folder)
+    results = AnnualDaylight(folder)
     results.average_values_to_folder(target_folder)
     average_values_folder = Path(target_folder, 'average_values')
     assert average_values_folder.is_dir()
@@ -112,7 +113,7 @@ def test_results_sample_average_values_to_folder():
 def test_results_sample_median_values_to_folder():
     folder = Path('./tests/assets/results_folders/results_sample')
     target_folder = Path('./tests/assets/temp')
-    results = Results(folder)
+    results = AnnualDaylight(folder)
     results.median_values_to_folder(target_folder)
     median_values_folder = Path(target_folder, 'median_values')
     assert median_values_folder.is_dir()
@@ -122,7 +123,7 @@ def test_results_sample_median_values_to_folder():
 def test_results_sample_cumulative_values_to_folder():
     folder = Path('./tests/assets/results_folders/results_sample')
     target_folder = Path('./tests/assets/temp')
-    results = Results(folder)
+    results = AnnualDaylight(folder)
     results.cumulative_values_to_folder(target_folder)
     cumulative_values_folder = Path(target_folder, 'cumulative_values')
     assert cumulative_values_folder.is_dir()
@@ -132,7 +133,7 @@ def test_results_sample_cumulative_values_to_folder():
 def test_results_sample_peak_values_to_folder():
     folder = Path('./tests/assets/results_folders/results_sample')
     target_folder = Path('./tests/assets/temp')
-    results = Results(folder)
+    results = AnnualDaylight(folder)
     results.peak_values_to_folder(target_folder)
     peak_values_folder = Path(target_folder, 'peak_values')
     assert peak_values_folder.is_dir()
@@ -141,7 +142,7 @@ def test_results_sample_peak_values_to_folder():
 
 def test_results_sample_annual_data():
     folder = Path('./tests/assets/results_folders/results_sample')
-    results = Results(folder)
+    results = AnnualDaylight(folder)
     data_collections, grids_info, sensor_index = results.annual_data()
     # length of data collections should be equal to grid count
     for count, grid_info in enumerate(grids_info):
@@ -151,7 +152,7 @@ def test_results_sample_annual_data():
 def test_results_sample_annual_data_to_folder():
     folder = Path('./tests/assets/results_folders/results_sample')
     target_folder = Path('./tests/assets/temp')
-    results = Results(folder)
+    results = AnnualDaylight(folder)
     grids_info = results.grids_info
     sensor_index = {}
     files = []
@@ -169,7 +170,7 @@ def test_results_sample_annual_data_to_folder():
 def test_results_sample_annual_data_to_folder_all_sensors():
     folder = Path('./tests/assets/results_folders/results_sample')
     target_folder = Path('./tests/assets/temp')
-    results = Results(folder)
+    results = AnnualDaylight(folder)
     grids_info = results.grids_info
     sensor_count = sum([grid_info['count'] for grid_info in grids_info])
     grids_info = results.grids_info
@@ -185,7 +186,7 @@ def test_results_sample_annual_data_to_folder_all_sensors():
 
 def test_results_sun_up_hours_to_annual():
     folder = Path('./tests/assets/results_folders/results_sample')
-    results = Results(folder)
+    results = AnnualDaylight(folder)
     sun_up_hours = results.sun_up_hours
     values = np.random.rand(len(sun_up_hours))
     annual_array = Results.values_to_annual(sun_up_hours, values, 1)
