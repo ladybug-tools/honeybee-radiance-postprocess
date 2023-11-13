a9ccd5ce7431ac145a725078d751a52815c72b06
Mikkel Pedersen <mkklpdrsn@gmail.com>
2023-11-13 16:38:00 +0100

    test(results): Change __static_apertures__ to __static__

--- tests/leed_test.py
+++ tests/leed_test.py
@@ -3,13 +3,13 @@ from pathlib import Path
 import json
 
 from ladybug.futil import nukedir
-from honeybee_radiance_postprocess.results import Results
+from honeybee_radiance_postprocess.results.annual_daylight import AnnualDaylight
 from honeybee_radiance_postprocess.leed import leed_option_one
 
 
 def test_results_leed_option_one_shade_transmittance_file():
     folder = './tests/assets/leed/results'
-    results = Results(folder)
+    results = AnnualDaylight(folder)
     shd_file = './tests/assets/leed/shd.json'
     with open(shd_file) as json_file:
         shade_transmittance = json.load(json_file)
@@ -21,7 +21,7 @@ def test_results_leed_option_one_shade_transmittance_file():
 
 def test_results_leed_option_one_shade_transmittance():
     folder = './tests/assets/leed/results'
-    results = Results(folder)
+    results = AnnualDaylight(folder)
     target_folder = Path('./tests/assets/temp/leed_summary')
     leed_option_one(results, shade_transmittance=0.02, sub_folder=target_folder)
     assert target_folder.is_dir()
