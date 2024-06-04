36e65f22cd4c962d7862a17188afa4de9a28a148
Mikkel Pedersen <mkklpdrsn@gmail.com>
2024-06-04 19:27:50 +0200

    fix(postprocess): Add command for ABNT NBR 15575

---
+++ honeybee_radiance_postprocess/cli/abnt.py
@@ -0,0 +1,116 @@
+"""Commands for ABNT NBR post-processing."""
+import json
+import sys
+import logging
+from pathlib import Path
+import click
+import numpy as np
+
+from honeybee.model import Model
+
+
+_logger = logging.getLogger(__name__)
+
+
+@click.group(help='Commands for ABNT NBR (Brazil) post-processing of Radiance results.')
+def abnt():
+    pass
+
+
+@abnt.command('abnt-nbr-15575')
+@click.argument(
+    'folder',
+    type=click.Path(exists=True, file_okay=False, dir_okay=True, resolve_path=True)
+)
+@click.argument('model-file', type=click.Path(
+    exists=True, file_okay=True, dir_okay=False, resolve_path=True))
+@click.option(
+    '--sub-folder', '-sf', help='Relative path for subfolder to write output '
+    'files.', default='abnt_nbr_15575'
+)
+def abnt_nbr_15575(
+    folder, model_file, sub_folder
+):
+    """
+
+    \b
+    Args:
+        folder:
+    """
+    def find_surrounding_points(x, y, x_coords, y_coords):
+        x1 = np.max(x_coords[x_coords <= x]) if np.any(x_coords <= x) else x_coords[0]
+        x2 = np.min(x_coords[x_coords > x]) if np.any(x_coords > x) else x_coords[-1]
+        y1 = np.max(y_coords[y_coords <= y]) if np.any(y_coords <= y) else y_coords[0]
+        y2 = np.min(y_coords[y_coords > y]) if np.any(y_coords > y) else y_coords[-1]
+        return x1, x2, y1, y2
+
+    def get_value(x, y, x_coords, y_coords, values):
+        index = np.where((x_coords == x) & (y_coords == y))
+        return values[index][0]
+
+    try:
+        folder = Path(folder)
+        hb_model = Model.from_file(model_file)
+        sensor_grids = hb_model.properties.radiance.sensor_grids
+        sg_full_identifier = {sg.full_identifier: sg for sg in sensor_grids}
+
+        folder_names = ['4_930AM', '4_330PM', '10_930AM', '10_330PM']
+        for _res_folder in folder_names:
+            res_folder = folder.joinpath(_res_folder)
+            with open(res_folder.joinpath('grids_info.json')) as data_f:
+                grids_info = json.load(data_f)
+            for grid_info in grids_info:
+                pit_values = \
+                    np.loadtxt(res_folder.joinpath(f'{grid_info["full_id"]}.res'))
+                sensor_grid = sg_full_identifier[grid_info['full_id']]
+                mesh_center = np.array([sensor_grid.mesh.center.x, sensor_grid.mesh.center.y])
+                sensor_points = np.array(
+                    [[sensor.pos[0], sensor.pos[1]] for sensor in sensor_grid.sensors])
+
+                x_coords = sensor_points[:, 0]
+                y_coords = sensor_points[:, 1]
+                x, y = mesh_center
+                x1, x2, y1, y2 = find_surrounding_points(x, y, x_coords, y_coords)
+                
+                # extract the illuminance values at the surrounding points
+                f_Q11 = get_value(x1, y1, x_coords, y_coords, pit_values) # bottom-left
+                f_Q21 = get_value(x2, y1, x_coords, y_coords, pit_values) # bottom-right
+                f_Q12 = get_value(x1, y2, x_coords, y_coords, pit_values) # top-left
+                f_Q22 = get_value(x2, y2, x_coords, y_coords, pit_values) # top-right
+                
+                # edge cases
+                if x == x1 and y == y1:
+                    f_xy = f_Q11
+                elif x == x2 and y == y1:
+                    f_xy = f_Q21
+                elif x == x1 and y == y2:
+                    f_xy = f_Q12
+                elif x == x2 and y == y2:
+                    f_xy = f_Q22
+                elif x1 == x2:
+                    # linear interpolation in y direction
+                    f_xy = f_Q11 + (f_Q12 - f_Q11) * (y - y1) / (y2 - y1)
+                elif y1 == y2:
+                    # linear interpolation in x direction
+                    f_xy = f_Q11 + (f_Q21 - f_Q11) * (x - x1) / (x2 - x1)
+                else:
+                    # perform bilinear interpolation
+                    f_xy = (f_Q11 * (x2 - x) * (y2 - y) +
+                            f_Q21 * (x - x1) * (y2 - y) +
+                            f_Q12 * (x2 - x) * (y - y1) +
+                            f_Q22 * (x - x1) * (y - y1)) / ((x2 - x1) * (y2 - y1))
+                
+                if f_xy >= 120:
+                    print('Superior', f_xy)
+                elif f_xy >= 90:
+                    print('Intermediario', f_xy)
+                elif f_xy >= 60:
+                    print('Minimo', f_xy)
+                else:
+                    print('Nao atende', f_xy)
+
+    except Exception:
+        _logger.exception('Failed to calculate ABNT NBR 15575 metrics.')
+        sys.exit(1)
+    else:
+        sys.exit(0)
