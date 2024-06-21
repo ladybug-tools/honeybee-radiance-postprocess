a251124975f657b7e9d99a8a10bb6a22b6575f6f
Mikkel Pedersen <mkklpdrsn@gmail.com>
2024-06-21 10:01:21 +0200

    fix(abnt): Add option for room center or grid center

--- honeybee_radiance_postprocess/vis_metadata.py
+++ honeybee_radiance_postprocess/vis_metadata.py
@@ -16,31 +16,31 @@ def _abnt_nbr_15575_daylight_levels_vis_metadata():
     colors = [Color(255, 198, 143), Color(255, 255, 209), Color(192, 231, 189), Color(83, 169, 206)]
     illuminance_levels_lpar = \
         LegendParameters(min=0, max=3, colors=colors, segment_count=4,
-                         title='Illuminance Level')
+                         title='Níveis de iluminamento')
     illuminance_levels_lpar.ordinal_dictionary = level_value
 
     metric_info_dict = {
         '4_930AM': {
             'type': 'VisualizationMetaData',
-            'data_type': GenericType('Illuminance April 23rd 9:30am', '').to_dict(),
+            'data_type': GenericType('23 de abril 9:30h', '').to_dict(),
             'unit': '',
             'legend_parameters': illuminance_levels_lpar.to_dict()
         },
         '4_330PM': {
             'type': 'VisualizationMetaData',
-            'data_type': GenericType('Illuminance April 23rd 3:30pm', '').to_dict(),
+            'data_type': GenericType('23 de abril 15:30h', '').to_dict(),
             'unit': '',
             'legend_parameters': illuminance_levels_lpar.to_dict()
         },
         '10_930AM': {
             'type': 'VisualizationMetaData',
-            'data_type': GenericType('Illuminance October 23rd 9:30am', '').to_dict(),
+            'data_type': GenericType('23 de outubro 9:30h', '').to_dict(),
             'unit': '',
             'legend_parameters': illuminance_levels_lpar.to_dict()
         },
         '10_330PM': {
             'type': 'VisualizationMetaData',
-            'data_type': GenericType('Illuminance October 23rd 3:30pm', '').to_dict(),
+            'data_type': GenericType('23 de outubro 15:30h', '').to_dict(),
             'unit': '',
             'legend_parameters': illuminance_levels_lpar.to_dict()
         }
