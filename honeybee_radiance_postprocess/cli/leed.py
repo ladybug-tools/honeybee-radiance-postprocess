017611f8b4801a32e9b4e7affbdd02093a0b3211
Mikkel Pedersen <mkklpdrsn@gmail.com>
2024-08-06 12:14:58 +0200

    fix(leed): Add option to use states in post-processing

--- honeybee_radiance_postprocess/cli/leed.py
+++ honeybee_radiance_postprocess/cli/leed.py
@@ -28,16 +28,24 @@ def leed():
 @click.option(
     '--shade-transmittance', '-st', help='A value to use as a multiplier in place of '
     'solar shading. Value for shade transmittance must be 1 > value > 0.',
-    default=0.05, show_default=True, type=click.FLOAT
+    default=0.02, show_default=True, type=click.FLOAT
 )
 @click.option(
     '--shade-transmittance-file', '-stf', help='A JSON file with a dictionary '
     'where aperture groups are keys, and the value for each key is the shade '
     'transmittance. Values for shade transmittance must be 1 > value > 0. '
     'If any aperture groups are missing in the JSON file, its shade transmittance '
-    'value will be set to the value of the shade transmittance option (0.05 by '
+    'value will be set to the value of the --shade-transmittance option (0.02 by '
     'default).', default=None, show_default=True,
     type=click.Path(exists=False, file_okay=True, dir_okay=False, resolve_path=True)
+)
+@click.option(
+    '--use-shade-transmittance/--use-states', help='A flag to select if the '
+    'post-processing should use a shade transmittance or the simulated states '
+    'of aperture groups. Using states should only be selected if the annual '
+    'daylight simulation included ray tracing of a second (blind) state for '
+    'each aperture group.',
+    is_flag=True, default=True, show_default=True
 )
 @click.option(
     '--sub-folder', '-sf', help='Relative path for subfolder to write output '
@@ -45,7 +53,7 @@ def leed():
 )
 def daylight_option_one(
     folder, shade_transmittance, shade_transmittance_file, grids_filter,
-    sub_folder
+    use_shade_transmittance, sub_folder
 ):
     """Calculate credits for LEED v4.1 Daylight Option 1.
 
@@ -60,7 +68,12 @@ def daylight_option_one(
         folder: Results folder. This folder is an output folder of annual daylight
             recipe. The daylight simulation must include aperture groups.
     """
-    if shade_transmittance_file and os.path.isfile(shade_transmittance_file):
+    use_states = not use_shade_transmittance
+    if (
+        shade_transmittance_file
+        and os.path.isfile(shade_transmittance_file)
+        and use_shade_transmittance
+    ):
         with open(shade_transmittance_file) as json_file:
             shd_trans = json.load(json_file)
         results = AnnualDaylight(folder)
@@ -73,7 +86,8 @@ def daylight_option_one(
     try:
         leed_option_one(
             folder, grids_filter=grids_filter,
-            shade_transmittance=shade_transmittance, sub_folder=sub_folder
+            shade_transmittance=shade_transmittance, use_states=use_states,
+            sub_folder=sub_folder
         )
     except Exception:
         _logger.exception('Failed to generate LEED summary.')
