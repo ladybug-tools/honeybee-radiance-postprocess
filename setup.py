05ebe2158020247a53a42de0d82dbf5cf6cd92e7
Chris Mackey <chris@ladybug.tools>
2025-08-24 11:52:56 -0700

    ci(release): Upgrade all dependencies used for release

--- setup.py
+++ setup.py
@@ -26,10 +26,13 @@ setuptools.setup(
         ]
     },
     classifiers=[
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
         "Programming Language :: Python :: Implementation :: CPython",
-        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent"
     ],
     license='GPLv3'
