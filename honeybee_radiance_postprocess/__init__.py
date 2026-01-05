5e611e116ec66c2270197dc3a7d3cc883e296e10
Mikkel Pedersen <mkklpdrsn@gmail.com>
2026-01-05 20:50:13 +0100

    fix(results): Add check for Python version in init

--- honeybee_radiance_postprocess/__init__.py
+++ honeybee_radiance_postprocess/__init__.py
@@ -1,26 +1,36 @@
 """honeybee-radiance-postprocess library."""
-try:
-    import cupy as np
-    np.arange(1)
-    IS_GPU = True
+import sys
+
+IS_GPU = False
+
+if sys.version_info >= (3, 0):
     try:
-        device_id = np.cuda.runtime.getDevice()
-        device_props = np.cuda.runtime.getDeviceProperties(device_id)
-        device_name = device_props['name'].decode()
-    except Exception:
-        device_name = 'Unknown GPU'
-    msg = (f'Using CuPy ({np.__version__}) for GPU ({device_name}) acceleration in '
-           'honeybee-radiance-postprocess.')
-    print(msg)
-except ModuleNotFoundError:
-    import numpy as np
-    IS_GPU = False
-except Exception as e:
-    import numpy as np
-    IS_GPU = False
-    msg = (f'Failed to load CuPy successfully. '
-           'Help: https://github.com/ladybug-tools/honeybee-radiance-postprocess/blob/master/CUPY.md. '
-           f'Falling back to NumPy ({np.__version__}) in honeybee-radiance-postprocess: {e}')
-    print(msg)
+        import cupy as np
+        np.arange(1)
+        IS_GPU = True
+        try:
+            device_id = np.cuda.runtime.getDevice()
+            device_props = np.cuda.runtime.getDeviceProperties(device_id)
+            device_name = device_props['name'].decode()
+        except Exception:
+            device_name = 'Unknown GPU'
+
+        msg = (
+            'Using CuPy ({}) for GPU ({}) acceleration in '
+            'honeybee-radiance-postprocess.'
+        ).format(np.__version__, device_name)
+        sys.stderr.write(msg)
+
+    except ModuleNotFoundError:
+        import numpy as np
+
+    except Exception as e:
+        import numpy as np
+        msg = (
+            'Failed to load CuPy successfully. '
+            'Help: https://github.com/ladybug-tools/honeybee-radiance-postprocess/blob/master/CUPY.md. '
+            'Falling back to NumPy ({}) in honeybee-radiance-postprocess: {}'
+        ).format(np.__version__, e)
+        sys.stderr.write(msg)
 
 IS_CPU = not IS_GPU
